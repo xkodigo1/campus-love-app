1b205e73297cea23750dec6e764eb3bf1aee3104
Fabian Galán <180666585+xkodigo1@users.noreply.github.com>
2025-05-16 07:11:20 -0500

    feat: :sparkles: create location interface

--- obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969ab53de3cd07d3df634defa27db7d73d155f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321d7e5a7dd4f6d7111bde8a84cbaa9e4b711812")]
 [assembly: System.Reflection.AssemblyProductAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
