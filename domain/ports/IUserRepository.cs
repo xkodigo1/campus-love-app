25d63197c295ba34719c955ee3ec6c09fe88a4e0
Fabian Galán <180666585+xkodigo1@users.noreply.github.com>
2025-05-16 06:29:07 -0500

    feat: create user interface

---
+++ domain/ports/IUserRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using campus_love_app.domain.entities;
+
+namespace campus_love_app.domain.ports
+{
+    public interface IUserRepository
+    {
+        void RegisterUser(User user);
+        List<User> GetAvailableProfiles(int userId);
+        void LikeUswer(int fromUserId, int toUserId);
+        List<User> GetMatches(int userId);
+    }
+}
--- obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941038ee00892feabebac7fc4b8f27f7a584477a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969ab53de3cd07d3df634defa27db7d73d155f45")]
 [assembly: System.Reflection.AssemblyProductAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
