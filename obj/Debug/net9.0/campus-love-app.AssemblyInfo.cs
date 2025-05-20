bbb14084a3ba0dead40c429b6536d4b1aaa172ad
Nico115-dev <181166596+Nico115-dev@users.noreply.github.com>
2025-05-19 21:34:09 -0500

    docs(json): :wrench:

--- obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/campus-love-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6005a407083e9da152571b019eaea25ad8929ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1707835077f0578d77acea8274ba1aab2a0c1c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("campus-love-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
